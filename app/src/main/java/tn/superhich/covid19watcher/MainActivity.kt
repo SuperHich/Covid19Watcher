d05df7f880866a69f1e6d54e1da274afb5f901de
hichem.laroussi <hichem.laroussi@tilia-aidants.fr>
2020-04-22 21:57:12 +0200

    HLA: Persist fragments

--- app/src/main/java/tn/superhich/covid19watcher/MainActivity.kt
+++ app/src/main/java/tn/superhich/covid19watcher/MainActivity.kt
@@ -1,37 +1,41 @@
 package tn.superhich.covid19watcher
 
 import android.os.Bundle
-import android.util.SparseArray
-import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import tn.superhich.covid19watcher.ui.dashboard.DashboardFragment
 import tn.superhich.covid19watcher.ui.home.HomeFragment
 import tn.superhich.covid19watcher.ui.notifications.NotificationsFragment
 
+
 class MainActivity : AppCompatActivity() {
 
-    private var savedStateSparseArray = SparseArray<Fragment.SavedState>()
-    private var currentSelectItemId = R.id.navigation_home
+    var homeFragment: HomeFragment = HomeFragment()
+    var dashboardFragment: DashboardFragment = DashboardFragment()
+    var notificationsFragment: NotificationsFragment = NotificationsFragment()
+    private var active : Fragment = homeFragment
 
-    companion object {
-        const val SAVED_STATE_CONTAINER_KEY = "ContainerKey"
-        const val SAVED_STATE_CURRENT_TAB_KEY = "CurrentTabKey"
-    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                swapFragments(item.itemId, getString(R.string.title_home))
+                supportFragmentManager.beginTransaction().hide(active).show(homeFragment).commit()
+                active = homeFragment
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                swapFragments(item.itemId, getString(R.string.title_dashboard))
+                supportFragmentManager.beginTransaction().hide(active).show(dashboardFragment).commit()
+                active = dashboardFragment
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                swapFragments(item.itemId, getString(R.string.title_notifications))
+                supportFragmentManager.beginTransaction().hide(active).show(notificationsFragment).commit()
+                active = notificationsFragment
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -40,14 +44,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState != null) {
-            savedStateSparseArray = savedInstanceState.getSparseParcelableArray(SAVED_STATE_CONTAINER_KEY)
-            currentSelectItemId = savedInstanceState.getInt(SAVED_STATE_CURRENT_TAB_KEY)
-        }
 
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-/*
+
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -55,58 +55,14 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-*/
+
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putSparseParcelableArray(SAVED_STATE_CONTAINER_KEY, savedStateSparseArray)
-        outState.putInt(SAVED_STATE_CURRENT_TAB_KEY, currentSelectItemId)
-    }
-
-    override fun onBackPressed() {
-        supportFragmentManager.fragments.forEach { fragment ->
-            if (fragment != null && fragment.isVisible) {
-                with(fragment.childFragmentManager) {
-                    if (backStackEntryCount > 0) {
-                        popBackStack()
-                        return
-                    }
-                }
-            }
-        }
-        super.onBackPressed()
-    }
-
-    private fun swapFragments(@IdRes actionId: Int, key: String) {
-        if (supportFragmentManager.findFragmentByTag(key) == null) {
-            savedFragmentState(actionId)
-            createFragment(key, actionId)
-        }
-    }
-
-    private fun createFragment(key: String, actionId: Int) {
-        val fragment = when (key) {
-            getString(R.string.title_home) -> HomeFragment()
-            getString(R.string.title_dashboard) -> DashboardFragment()
-            else -> NotificationsFragment()
+        with(supportFragmentManager.beginTransaction()) {
+            add(R.id.nav_host_fragment, notificationsFragment, getString(R.string.title_notifications)).hide(notificationsFragment)
+            add(R.id.nav_host_fragment, dashboardFragment, getString(R.string.title_dashboard)).hide(dashboardFragment)
+            add(R.id.nav_host_fragment, homeFragment, getString(R.string.title_home)).commit()
         }
 
-        fragment.setInitialSavedState(savedStateSparseArray[actionId])
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment, fragment, key)
-            .commit()
-    }
-
-    private fun savedFragmentState(actionId: Int) {
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        if (currentFragment != null) {
-            savedStateSparseArray.put(currentSelectItemId,
-                supportFragmentManager.saveFragmentInstanceState(currentFragment)
-            )
-        }
-        currentSelectItemId = actionId
     }
 }
